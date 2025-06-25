5d6b0387414475c73f58e750a5c5697c09ebcdde
Zack Sargent <zack@zack.fyi>
2025-06-25 15:43:10 -0400

    add length restrictions

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,6 +2,7 @@ class Author < ApplicationRecord
   # https://stackoverflow.com/a/31062521
   validates_format_of :website, :with => /\A(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?\Z/i,
     allow_blank: true
+  validates :name, length: { maximum: 30 }
 
   def display_name
     name.presence || "Anonymous"
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,9 @@ class Comment < ApplicationRecord
   belongs_to :parent, class_name: 'Comment', optional: true
   has_many :replies, class_name: 'Comment', foreign_key: 'parent_id', dependent: :destroy
 
-  validates :content, presence: true
+  MAXIMUM_COMMENT_LENGTH = 10_000 # characters
+
+  validates :content, presence: true, length: { maximum: MAXIMUM_COMMENT_LENGTH }
   validates :author, presence: true
   validates :post_path, presence: true
 
