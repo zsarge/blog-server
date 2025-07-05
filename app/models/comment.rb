97c3f7465326575dafd66b2bf332389076fe4b38
Zack Sargent <zack@zack.fyi>
2025-07-05 15:30:21 -0400

    actually, 10k is a lot

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,7 @@ class Comment < ApplicationRecord
   belongs_to :parent, class_name: 'Comment', optional: true
   has_many :replies, class_name: 'Comment', foreign_key: 'parent_id', dependent: :destroy
 
-  MAXIMUM_COMMENT_LENGTH = 10_000 # characters
+  MAXIMUM_COMMENT_LENGTH = 5_000 # characters
 
   validates :content, presence: true, length: { maximum: MAXIMUM_COMMENT_LENGTH }
   validates :author, presence: true
