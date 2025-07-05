dcd6567bdd456813afabec398dd03f39a0d07c40
Zack Sargent <zack@zack.fyi>
2025-07-05 13:56:16 -0400

    use absolute path in systemctl script

--- nginx/start-server.sh
+++ nginx/start-server.sh
@@ -1,9 +1,4 @@
 #!/bin/env bash
 
-set -e # stop immediately on errors
-set -o pipefail # do not silently ignore errors in pipelines
-cd "${0%/*}" # cd into script's current location
-cd .. # go into root dir
-
-RAILS_ENV=production bundle exec puma -C config/puma.rb
+RAILS_ENV=production /home/blog-server/.asdf/shims/bundle exec puma -C config/puma.rb
 
