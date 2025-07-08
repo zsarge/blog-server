55729cd9611725a7af54d65e46919f7bd6677cb0
Zack Sargent <zack@zack.fyi>
2025-07-08 09:06:17 -0400

    remote files controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,6 @@ Rails.application.routes.draw do
 
   resources :authors
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "/files/:collection", to: "files#show_collection"
-  get "/files", to: "files#show_image"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
